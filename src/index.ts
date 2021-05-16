6ad9eb285d76325e2d90425dda335cd0871c064a
christopher-caldwell <caldwell8814@gmail.com>
2021-05-15 18:57:26 -0600

    separating utilities

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import { Options } from './interfaces'
 import { formatLong, formatShort, parse } from './helpers'
 
-/**
- * Parse or format the given `val`.
- */
-export const ms = (val: string | number, { long, preferredUnit }: Options = {}): string | number | undefined => {
-  const type = typeof val
-  if (type === 'string' && (val as string).length > 0) return parse(val as string)
+/** Convert a logical time unit to milliseconds */
+export const convertTimeToMs = (time: string): number | undefined => {
+  if (typeof time === 'string' && time.length > 0) return parse(time as string)
+  throw new Error('val is not a non-empty string or a valid number. val=' + JSON.stringify(time))
+}
 
-  if (type === 'number' && isFinite(val as number))
-    return long ? formatLong(val as number, preferredUnit) : formatShort(val as number, preferredUnit)
-
-  throw new Error('val is not a non-empty string or a valid number. val=' + JSON.stringify(val))
+/** Convert milliseconds to a logical time unit  */
+export const convertMsToTime = (ms: number, { long, preferredUnit }: Options = {}): string | undefined => {
+  if (typeof ms !== 'number' || !isFinite(ms)) {
+    throw new Error('val is not a non-empty string or a valid number. val=' + JSON.stringify(ms))
+  }
+  return long ? formatLong(ms, preferredUnit) : formatShort(ms, preferredUnit)
 }
 
 export * from './interfaces'
--- tests/invalid.spec.ts
+++ tests/invalid.spec.ts
@@ -1,63 +1,97 @@
-import { ms } from '@/index'
+import { convertMsToTime, convertTimeToMs } from '@/index'
 import { maximumInputLength } from '@/helpers'
 
 describe('ms(invalid inputs)', () => {
   it('should throw an error, when ms("")', () => {
     expect(() => {
       //@ts-ignore
-      ms('')
+      convertTimeToMs('')
+    }).toThrowError()
+    expect(() => {
+      //@ts-ignore
+      convertMsToTime('')
     }).toThrowError()
   })
 
   it('should return undefined when the value is longer than 100 characters', () => {
     const tooLongInput = new Array(maximumInputLength + 1).fill('a').reduce((acc, current) => acc + current, '')
-    ms(tooLongInput)
-    expect(ms(tooLongInput)).toBe(undefined)
+    expect(convertTimeToMs(tooLongInput)).toBe(undefined)
   })
 
   it('should throw an error, when ms(undefined)', () => {
     expect(() => {
       //@ts-ignore
-      ms(undefined)
+      convertMsToTime(undefined)
+    }).toThrowError()
+    expect(() => {
+      //@ts-ignore
+      convertTimeToMs(undefined)
     }).toThrowError()
   })
 
   it('should throw an error, when ms(null)', () => {
     expect(() => {
       //@ts-ignore
-      ms(null)
+      convertMsToTime(null)
+    }).toThrowError()
+    expect(() => {
+      //@ts-ignore
+      convertTimeToMs(null)
     }).toThrowError()
   })
 
   it('should throw an error, when ms([])', () => {
     expect(() => {
       //@ts-ignore
-      ms([])
+      convertMsToTime([])
+    }).toThrowError()
+    expect(() => {
+      //@ts-ignore
+      convertTimeToMs([])
     }).toThrowError()
   })
 
   it('should throw an error, when ms({})', () => {
     expect(() => {
       //@ts-ignore
-      ms({})
+      convertMsToTime({})
+    }).toThrowError()
+    expect(() => {
+      //@ts-ignore
+      convertTimeToMs({})
     }).toThrowError()
   })
 
   it('should throw an error, when ms(NaN)', () => {
     expect(() => {
-      ms(NaN)
+      //@ts-ignore
+      convertMsToTime(NaN)
+    }).toThrowError()
+    expect(() => {
+      //@ts-ignore
+      convertTimeToMs(NaN)
     }).toThrowError()
   })
 
   it('should throw an error, when ms(Infinity)', () => {
     expect(() => {
-      ms(Infinity)
+      //@ts-ignore
+      convertMsToTime(Infinity)
+    }).toThrowError()
+    expect(() => {
+      //@ts-ignore
+      convertTimeToMs(Infinity)
     }).toThrowError()
   })
 
   it('should throw an error, when ms(-Infinity)', () => {
     expect(() => {
-      ms(-Infinity)
+      //@ts-ignore
+      convertMsToTime(-Infinity)
+    }).toThrowError()
+    expect(() => {
+      //@ts-ignore
+      convertTimeToMs(-Infinity)
     }).toThrowError()
   })
 })
