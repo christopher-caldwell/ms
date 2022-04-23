319dcdffa69a0db9975c9935a1ba9ed340b7d6e4
Christopher Caldwell <62559469+christopher-caldwell@users.noreply.github.com>
2022-04-23 09:08:48 -0500

    fix: Removing fragment

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,25 +1,24 @@
 import { FC } from 'react'
 import { Typography, Grid, styled } from '@mui/material'
+
 import { TimeToMs, MsToTime, FutureDateTime, FutureDateMs } from './features'
 
 const App: FC = () => {
   return (
-    <>
-      <Root>
-        <Grid container sx={{ padding: '0 50px' }}>
-          <Grid item xs={12}>
-            <Typography variant='h4'>@caldwell619/ms</Typography>
-            <Typography sx={{ marginBottom: '10vh' }} variant='subtitle1'>
-              Tiny time formatting utility forked from vercel/ms
-            </Typography>
-          </Grid>
-          <TimeToMs />
-          <MsToTime />
-          <FutureDateMs />
-          <FutureDateTime />
+    <Root>
+      <Grid container sx={{ padding: '0 50px' }}>
+        <Grid item xs={12}>
+          <Typography variant='h4'>@caldwell619/ms</Typography>
+          <Typography sx={{ marginBottom: '10vh' }} variant='subtitle1'>
+            Tiny time formatting utility forked from vercel/ms
+          </Typography>
         </Grid>
-      </Root>
-    </>
+        <TimeToMs />
+        <MsToTime />
+        <FutureDateMs />
+        <FutureDateTime />
+      </Grid>
+    </Root>
   )
 }
 
